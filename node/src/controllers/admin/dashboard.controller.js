65e1ec75e81ccdac929a77c9223e2f6012aaf6fc
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-07-06 23:07:39 +0700

    thêm giao diện và data cho trang dashboard bên admin

--- node/src/controllers/admin/dashboard.controller.js
+++ node/src/controllers/admin/dashboard.controller.js
@@ -1,10 +1,92 @@
 
+const Productcategory=require("../../models/products-category-model")
+const Product=require("../../models/products")
+const Client=require("../../models/user.model")
+const Admin=require("../../models/account")
+const admin= async (req, res)=>{
+
+const static={
+    categoryProduct:{
+        total:0,
+        active:0,
+        inactive:0
+    },
+    product:{
+        total:0,
+        active:0,
+        inactive:0
+    },
+    client:{
+        total:0,
+        active:0,
+        inactive:0
+    },
+    admin:{
+        total:0,
+        active:0,
+        inactive:0
+    }
+    
+}
+
+static.categoryProduct.total=await Productcategory.countDocuments({
+    deleted:false
+})
+static.categoryProduct.active=await Productcategory.countDocuments({
+    deleted:false,
+    status:"active"
+})
+static.categoryProduct.inactive=await Productcategory.countDocuments({
+    deleted:false,
+    status:"inactive"
+})
+
+static.product.total=await Product.countDocuments({
+    deleted:false
+})
+static.product.active=await Product.countDocuments({
+    deleted:false,
+    status:"active"
+})
+static.product.inactive=await Product.countDocuments({
+    deleted:false,
+    status:"inactive"
+})
+
+static.client.total=await Client.countDocuments({
+    deleted:false
+})
+static.client.active=await Client.countDocuments({
+    deleted:false,
+    status:"active"
+})
+static.client.inactive=await Client.countDocuments({
+    deleted:false,
+    status:"inactive"
+})
+
+static.admin.total=await Admin.countDocuments({
+    deleted:false
+})
+static.admin.active=await Admin.countDocuments({
+    deleted:false,
+    status:"active"
+})
+static.admin.inactive=await Admin.countDocuments({
+    deleted:false,
+    status:"inactive"
+})
+
+    
 
 
-const admin=(req, res)=>{
+
     res.render('admin/pages/dashboard/index.pug', {
         title:"Sản phẩm nổi bật",
-        titles:"Sản phẩm mới"
+        titles:"Sản phẩm mới",
+        static:static,
+        message: req.flash('message'),
+        error: req.flash('error')
     });
 }
 
